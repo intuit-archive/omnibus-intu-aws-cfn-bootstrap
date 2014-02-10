be6b6dbd5a2388f13d1f5a288618906e5fe59797
Chad Cloes <chad_cloes@intuit.com>
2014-02-10 14:33:21 -0800

    Updated for working code

--- config/software/intu-aws-cfn-bootstrap.rb
+++ config/software/intu-aws-cfn-bootstrap.rb
@@ -5,13 +5,8 @@ dependencies ["openssl", "python", "virtualenv"]
 
 build do
   command "if rpm -qa|grep aws-cfn-bootstrap;then rpm -e --quiet `rpm -qa --queryformat '%{name}\n'|grep aws-cfn-bootstrap`;fi"
-  cmd =  "source #{install_dir}/bin/activate;"
-  cmd << "easy_install requests"
-  cmd << "easy_install python-daemon"
-  cmd << "wget https://s3.amazonaws.com/cloudformation-examples/aws-cfn-bootstrap-latest.tar.gz"
-  cmd << "tar xzvf aws-cfn-bootstrap-latest.tar.gz"
-  cmd << "cd aws-cfn-bootstrap-1.3"
-  cmd << "python build setup.py"
-  cmd << "python install setup.py" 
-  command cmd
+  command "source #{install_dir}/bin/activate"
+  command "easy_install requests"
+  command "easy_install python-daemon"
+  command "easy_install https://s3.amazonaws.com/cloudformation-examples/aws-cfn-bootstrap-latest.tar.gz"
 end
