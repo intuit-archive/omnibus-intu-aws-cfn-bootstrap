1f45ed53790d2f08e42825f515e61c4b7628619f
Chad Cloes <chad_cloes@intuit.com>
2014-02-11 09:09:39 -0800

    Updated to reduce duplicate code per peer review

--- config/software/intu-aws-cfn-bootstrap.rb
+++ config/software/intu-aws-cfn-bootstrap.rb
@@ -3,8 +3,9 @@ version ENV['version']
 
 dependencies ["openssl", "python", "virtualenv"]
 
+pip_install_list = ["requests==2.2.1", "python-daemon==1.6", "https://s3.amazonaws.com/cloudformation-examples/aws-cfn-bootstrap-latest.tar.gz"]
+
 build do
-  command ". #{install_dir}/bin/activate; pip install -v --install-option='--prefix=#{install_dir}' requests==2.2.1"
-  command ". #{install_dir}/bin/activate; pip install -v --install-option='--prefix=#{install_dir}' python-daemon==1.6"
-  command ". #{install_dir}/bin/activate; pip install -v --install-option='--prefix=#{install_dir}' https://s3.amazonaws.com/cloudformation-examples/aws-cfn-bootstrap-latest.tar.gz"
+  pip_install_list.each do |p|
+    command ". #{install_dir}/bin/activate; pip install -v --install-option='--prefix=#{install_dir}' #{p}"
 end
