06590eea22aec089f465253607ff92adfef42479
Chad Cloes <chad_cloes@intuit.com>
2014-02-11 09:50:50 -0800

    Fixing syntax to include the required end

--- config/software/intu-aws-cfn-bootstrap.rb
+++ config/software/intu-aws-cfn-bootstrap.rb
@@ -8,4 +8,5 @@ pip_install_list = ["requests==2.2.1", "python-daemon==1.6", "https://s3.amazona
 build do
   pip_install_list.each do |p|
     command ". #{install_dir}/bin/activate; pip install -v --install-option='--prefix=#{install_dir}' #{p}"
+  end
 end
